78595d252a91550f43144cb5b0da9eba904a7a25
Timo Adam <ta59@st-andrews.ac.uk>
2020-10-29 11:37:19 +0000

    Update FitMarkovSwitchingGAMLSS.R

--- R/FitMarkovSwitchingGAMLSS.R
+++ R/FitMarkovSwitchingGAMLSS.R
@@ -11,17 +11,17 @@ library(gridExtra)
 library(RColorBrewer)
 
 ## Function that fits the model
-fit.msgamboostlss = function(x, y, N = 2, type = "ms-gamlss", stat = FALSE, m.stop = rep(200, 2), max.iter = 50, conv.tol = 1e-03, conv.print = TRUE) { 
-  x1 = x[,1]
+FitMarkovSwitchingGAMLSS <- function(x, y, N = 2, type = "MSGAMLSS", stat = FALSE, m.stop = rep(200, 2), max.iter = 50, conv.tol = 1e-03, conv.print = TRUE) { 
+  x1 = x[, 1]
   delta = NULL
-  gamma = matrix(c(0.95, 0.05, 0.05, 0.95), ncol=2)
-  mod = fv.mu = fv.sigma = vector("list")
+  gamma = matrix(c(0.95, 0.05, 0.05, 0.95), ncol = 2)
+  mod = fv.mu = fv.sigma = list()
   term = FALSE
   old = 0
   circ.crit = 0
   for(j in 1:N) {
     fv.mu[[j]] = rep(c(3, 7)[j], length(y))
-    fv.sigma[[j]]=rep(3, length(y));
+    fv.sigma[[j]] = rep(3, length(y))
   }
   while(term == FALSE) {
     for(i in 1:max.iter) {
@@ -42,67 +42,67 @@ fit.msgamboostlss = function(x, y, N = 2, type = "ms-gamlss", stat = FALSE, m.st
       sumfoo = sum(foo)
       lscale = log(sumfoo)
       foo = foo / sumfoo
-      lalpha[,1] = log(foo) + lscale
+      lalpha[, 1] = log(foo) + lscale
       for(j in 2:length(y)) {
         foo = foo %*% gamma * allprobs[j,]
         sumfoo = sum(foo)
         lscale = lscale + log(sumfoo)
         foo = foo / sumfoo
-        lalpha[,j] = log(foo) + lscale
+        lalpha[, j] = log(foo) + lscale
       }
       foo = rep(1 / N, N)
-      lbeta[,length(y)] = rep(0, N)
+      lbeta[, length(y)] = rep(0, N)
       lscale = log(N)
       foo = foo / sum(foo)
       for(j in (length(y) - 1):1) {
         foo = gamma %*% (allprobs[j + 1,] * foo)
-        lbeta[,j] = log(foo) + lscale
+        lbeta[, j] = log(foo) + lscale
         sumfoo = sum(foo)
         foo = foo / sumfoo
         lscale = lscale + log(sumfoo)
       }                   
       lallprobs = log(allprobs)
-      llh = max(lalpha[,length(y)]) + log(sum(exp(lalpha[,length(y)] - max(lalpha[,length(y)]))))
+      llh = max(lalpha[, length(y)]) + log(sum(exp(lalpha[, length(y)] - max(lalpha[, length(y)]))))
       weights = matrix(NA, N, length(y))
       for(j in 1:length(y)) {
-        weights[,j] = exp(lalpha[,j] + lbeta[,j] - llh)
+        weights[,j] = exp(lalpha[,j] + lbeta[, j] - llh)
       }
       for(j in 1:N) {
         for(k in 1:N) {
-          gamma.next[j,k] = gamma[j,k] * sum(exp(lalpha[j,1:(length(y) - 1)] + lallprobs[2:length(y),k] + lbeta[k,2:length(y)] - llh))
+          gamma.next[j, k] = gamma[j, k] * sum(exp(lalpha[j, 1:(length(y) - 1)] + lallprobs[2:length(y), k] + lbeta[k, 2:length(y)] - llh))
         }
       }
       gamma.next = gamma.next / apply(gamma.next, 1, sum)
       if(stat == TRUE) {
         delta.next = solve(t(diag(N) - gamma.next + 1), rep(1, N))
       }else{
-        delta.next = exp(lalpha[,1] + lbeta[,1] - llh)
+        delta.next = exp(lalpha[, 1] + lbeta[, 1] - llh)
         delta.next = delta.next / sum(delta.next)
       }
       conv.crit = sum(abs(gamma - gamma.next)) + sum(abs(delta - delta.next))
       ind = weights
       for(j in 1:N){
-        if(type=="ms-glmlss"){
+        if(type == "MSGLMLSS") {
           mod[[j]] = glmboostLSS(y ~ x1, weights = ind[j,], data = data.frame(x1 = x1, y = y), families = as.families("NO"), method = "noncyclic", control = boost_control(mstop = m.stop[j], nu = 0.1))
         }
-        if(type=="ms-gamlss"){
+        if(type == "MSGAMLSS") {
           mod[[j]] = gamboostLSS(y ~ x1, weights = ind[j,], data = data.frame(x1 = x1, y = y), families = as.families("NO"), method = "noncyclic", control = boost_control(mstop = m.stop[j], nu = 0.1))
         }
         fv.mu.next[[j]] = as.vector(fitted(mod[[j]]$mu))
         fv.sigma.next[[j]] = as.vector(fitted(mod[[j]]$sigma))
       }
       conv.crit = abs(llh - old)
-      if(conv.print == TRUE){
-        cat("Iteration = ", i, ", criterion = ", round(conv.crit, 3), "\r");
+      if(conv.print == TRUE) {
+        cat("Iteration = ", i, ", criterion = ", round(conv.crit, 3), "\r")
       }
-      if(conv.crit<conv.tol|(conv.crit<1&abs(conv.crit - circ.crit)<1e-09)|i == max.iter){
-        if(i==max.iter){
-          print(paste("No convergence within", max.iter, "iterations"));
+      if(conv.crit < conv.tol|(conv.crit<1&abs(conv.crit - circ.crit) < 1e-09)|i == max.iter) {
+        if(i == max.iter) {
+          print(paste("No convergence within", max.iter, "iterations"))
         }else{
-          print(paste("Convergence after", i, "iterations"));
+          print(paste("Convergence after", i, "iterations"))
         }
-        term = TRUE;
-        break;
+        term = TRUE
+        break
       }  
       delta = delta.next
       gamma = gamma.next
